e68bf262cc7802af39fe38e1bb1a4bd67941559c
Carolyn Lam <caro_lyn_@hotmail.com>
2016-11-15 11:47:53 -0500

    add different logic versions to repeat_string_num_times.js

--- repeat_string_num_times.js
+++ repeat_string_num_times.js
@@ -1,6 +1,7 @@
 // if num is a positive number, return a string repeated num times
 // else, return an empty string
 
+// Version 1 using repeat method
 function repeatStringNumTimes(str, num) {
   if (num > 0) {
     return str.repeat(num);
@@ -10,3 +11,28 @@ function repeatStringNumTimes(str, num) {
 }
 
 console.log(repeatStringNumTimes("abc", 3));
+
+// Version 2 using a for loop and a ternary operator to return value
+// function repeatStringNumTimes(str, num) {
+//   var newStr = "";
+//   if (num > 0) {
+//     for (var i = 0; i < num; i++) {
+//       newStr += str;
+//     }
+//     return newStr;
+//   }
+//   return (newStr.length > 0 ? newStr : "");
+// }
+
+// Version 3 using a for loop
+// function repeatStringNumTimes(str, num) {
+//   var newStr = "";
+//   if (num > 0) {
+//     for (var i = 0; i < num; i++) {
+//       newStr += str;
+//     }
+//     return newStr;
+//   } else {
+//   return "";
+//   }
+// }
