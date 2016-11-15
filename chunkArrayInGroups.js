9d52289c9a97da504f8f7f3774f54e1e0b625fee
Carolyn Lam <caro_lyn_@hotmail.com>
2016-11-15 13:56:52 -0500

    refactor chunkArrayInGroups.js logic

--- chunkArrayInGroups.js
+++ chunkArrayInGroups.js
@@ -1,26 +1,45 @@
 // write a function that splits an array into another array whose
 // length === size
-// return it as a 2-dimensional array 
+// return it as a 2-dimensional array
 
 function chunkArrayInGroups(arr, size) {
   var mamaArr = [];
-  var childArr = [];
+  var babyArr = [];
 
-  for(var i = 1; i <= arr.length; i++) {
-    childArr.push(arr[i - 1]);
+  for (var i = 1; i <= arr.length; i++) {
+    babyArr.push(arr[i - 1]);
     if (i % size === 0) {
-      // push into arr when length === size
-      mamaArr.push(childArr);
-      // clear
-      childArr = [];
+      mamaArr.push(babyArr);
+      babyArr = [];
+    }
   }
-  if (childArr.length > 0) {
-    mamaArr.push(childArr);
+  if (babyArr.length > 0) {
+    mamaArr.push(babyArr);
   }
   return mamaArr;
 }
 
-console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4));
+
+// function chunkArrayInGroups(arr, size) {
+//   var mamaArr = [];
+//   var childArr = [];
+//
+//   for(var i = 1; i <= arr.length; i++) {
+//     childArr.push(arr[i - 1]);
+//     if (i % size === 0) {
+//       // push into arr when length === size
+//       mamaArr.push(childArr);
+//       // clear
+//       childArr = [];
+//   }
+//   if (childArr.length > 0) {
+//     mamaArr.push(childArr);
+//   }
+//   return mamaArr;
+// }
+//
+// console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3));
 
 
 
@@ -62,8 +81,6 @@ console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3));
 //   }
 //   return mamaArr;
 // }
-
-console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4));
 
 // function chunkArrayInGroups(arr, size) {
 //   var mamaArr = [];
